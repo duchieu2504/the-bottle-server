42f3ebbcf175529b2cd2fa49bdeeece9caf8161f
I'm Fox <zero250401@gmail.com>
2022-09-07 17:23:55 +0700

    update connect db

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,12 +1,22 @@
 import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+dotenv.config();
 
+console.log(process.env.USERNAMEMONGDB);
+console.log(process.env.PASSWORD);
 // kết nối với mongoDB
 const connect = async () => {
     try {
         await mongoose.connect(
-            process.env.MONGODB_URI ||
-                "mongodb://127.0.0.1:27017/myshop-backend"
+            `mongodb+srv://${process.env.USERNAMEMONGDB}:${process.env.PASSWORD}@the-bottle.ujjp7e3.mongodb.net/?retryWrites=true&w=majority`,
+            {
+                // useCreateIndex: true,
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                // useFindAndModify: false,
+            }
         );
+        console.log("MongoDB connected");
     } catch (err) {
         console.log("Connect failure");
     }
